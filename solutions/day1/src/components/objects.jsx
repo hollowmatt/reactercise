7cdaae779c54b106c308d6bec0ee9ab976d16a53
Matt Holloway <hollowmatt@gmail.com>
2023-01-14 14:52:43 -0500

    day 1 done enough

--- solutions/day1/src/components/objects.jsx
+++ solutions/day1/src/components/objects.jsx
@@ -1,16 +1,7 @@
 export function Objects() {
-  const dog = {};
-  dog.name = "Logan";
-  dog.legs = 4;
-  dog.color = "whilte";
-  dog.age = 5;
-  dog.bark = function() {
-    return "woof, woof";
-  };
-  dog.breed = 'Greyhound';
-  dog.getDogInfo = function() {
-    return(`${this.name} is a ${this.age} year old ${this.breed}, with a ${this.color} coat, and ${this.legs} legs.`);
-  }
-  console.log(dog.bark());
-  console.log(dog.getDogInfo());
+  const countries = ['Finland', 'Estonia', 'Sweden', 'Norway', 'Iceland'];
+  const countriesWithLand = countries.filter((country) => 
+    country.includes('land')
+  );
+  console.log(countriesWithLand);
 }
