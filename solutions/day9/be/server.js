2e7af1797aa50c9ad675f4389b77c8b2041d5402
Matt Holloway <hollowmatt@gmail.com>
2023-01-22 16:52:38 -0500

    add routes for users and exercises

--- solutions/day9/be/server.js
+++ solutions/day9/be/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
 require('dotenv').config();
 
 const app = express();
@@ -14,6 +16,9 @@ const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connection established");
 });
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Express server runnign on port: ${port}`);
