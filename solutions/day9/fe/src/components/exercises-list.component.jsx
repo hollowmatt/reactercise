661d0bf44d0335d83af780480710d3ae7a6c9016
Matt Holloway <hollowmatt@gmail.com>
2023-01-22 20:26:56 -0500

    started to pull data into the views for user list and exercices list

--- solutions/day9/fe/src/components/exercises-list.component.jsx
+++ solutions/day9/fe/src/components/exercises-list.component.jsx
@@ -1,8 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const EXERCISES_URL = "http://localhost:3030/exercises";
+
 function ExercisesList () {
+  const [loading, isLoading] = useState(true);
+  const [exercises, setExercises] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    fetch(EXERCISES_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setExercises(jsonResponse);
+        isLoading(false);
+      })
+  }, []);
+
   return(
-    <p>
-      Placeholder for List of Exercises,
-    </p>
+    <div>
+      {loading && !errorMsg ? (
+        <span>loading ...</span>
+      ) : errorMsg ? (
+        <span color="red">{errorMsg}</span>
+      ) : (
+        exercises.map((exercise, index) => (
+          <p>{exercise.description}</p>
+        ))
+      )}
+    </div>
   )
 };
 
--- solutions/day9/fe/src/components/users-list.component.jsx
+++ solutions/day9/fe/src/components/users-list.component.jsx
@@ -1,8 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const USERS_URL = "http://localhost:3030/users";
+
 function UsersList () {
+  const [loading, isLoading] = useState(true);
+  const [users, setUsers] = useState([]);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    fetch(USERS_URL)
+      .then(response => response.json())
+      .then(jsonResponse => {
+        setUsers(jsonResponse);
+        isLoading(false);
+      })
+  }, []);
+
   return(
-    <p>
-      Placeholder for List of Users,
-    </p>
+    <div>
+      {loading && !errorMsg ? (
+        <span>loading...</span>
+      ) : errorMsg ? (
+        <span color="red">{errorMsg}</span>
+      ) : (
+        users.map((user, index) => (
+          <p>{user.username}</p>
+        ))
+      )}
+    </div>
   )
 };
 
