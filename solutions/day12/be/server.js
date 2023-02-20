6a4b006f1707f3105753d340cfbda564307263aa
Matt Holloway <hollowmatt@gmail.com>
2023-02-20 17:50:05 -0400

    setup structure of the backend (Node/Express)

---
+++ solutions/day12/be/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Express server running on port: ${port}`);
+});
