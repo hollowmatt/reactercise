bd56792d50c373befbf9a799df04fcfc26c8bd06
Matt Holloway <hollowmatt@gmail.com>
2023-02-22 19:21:28 -0500

    implement styles, update header and progress

--- solutions/day12/fe/src/components/Header.js
+++ solutions/day12/fe/src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Progress from './Progress';
 
 function Header() {
   return(
     <div>
       <h1>Multi Step Registration</h1>
+      <Progress/>
     </div>
   );
 };
